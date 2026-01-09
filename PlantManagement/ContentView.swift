066ca0a9c6d890cfe83c46f88e3ba298ce6a517f
mchancfy <marychancfy@gmail.com>
2026-01-09 21:21:28 +1100

    Update ContentView text

--- PlantManagement/ContentView.swift
+++ PlantManagement/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, plant!")
+            Text("Hello, Plant Management 🌱！")
         }
         .padding()
     }
