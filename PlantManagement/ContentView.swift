ea7220111d0855c4f67a2ded425470331f2ebed0
mchancfy <marychancfy@gmail.com>
2026-01-09 16:15:47 +1100

    Initial Commit

---
+++ PlantManagement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlantManagement
+//
+//  Created by Mary Chan on 9/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlantManagement/PlantManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantManagementApp.swift
+//  PlantManagement
+//
+//  Created by Mary Chan on 9/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlantManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
